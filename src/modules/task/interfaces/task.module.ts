b24a3398642e072ae00a8f02008e91fb5b4d84d0
BryanArredon <byemilio2005@gmail.com>
2026-02-09 20:06:19 -0600

    Configuracion de CRUD de tareas

--- src/modules/task/interfaces/task.module.ts
+++ src/modules/task/interfaces/task.module.ts
@@ -3,7 +3,6 @@ import { TaskController } from "./task.controller";
 import { TaskService } from "./task.service";
 
 @Module({
-    imports: [TaskModule],
     controllers: [TaskController],
     providers: [TaskService] 
 })
--- src/modules/task/interfaces/task.service.ts
+++ src/modules/task/interfaces/task.service.ts
@@ -3,7 +3,22 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class TaskService {
     
-    public login (): string{
+    public getTask (): string{
         return "Listado de tareas"; 
     }
+
+    public getTaskById (id: number): string{
+        return `Se obtiene la tarea ${id}`;
+
+    }
+    public insertTask (task: any): any{
+        return task;
+    }
+    public updateTask (id: number, task: any): any{
+        return task;
+    }
+    public deleteTask (id: number): string{
+        return `Eliminando la tarea ${id}`;
+    }
+
 }
